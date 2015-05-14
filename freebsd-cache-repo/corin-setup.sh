e633cdb437c4daf84aea181b196564e400ef5a51
Marko Kosmerl <marko.kosmerl@gmail.com>
2015-05-13 17:03:18 -0700

    Adding corin and tamara setups

--- freebsd-cache-repo/corin-setup.sh
+++ freebsd-cache-repo/corin-setup.sh
@@ -1,3 +1,15 @@
 echo "Corin is up and running!"
 
-#TODO: clone dev repo and test
+mkdir proj
+cd proj
+cp /vagrant/config/composer/composer.json .
+cp /vagrant/config/private-bower/bower.json .
+cp /vagrant/config/sinopia/package.json .
+cp /vagrant/config/private-bower/.bowerrc .
+
+echo "Setting npm registry"
+npm set registry "http://10.0.0.2:4873"
+
+chown -R vagrant .
+
+
--- freebsd-cache-repo/tamara-setup.sh
+++ freebsd-cache-repo/tamara-setup.sh
@@ -1,9 +1,21 @@
 echo "Tamara is up and running!"
 
-echo "Mounting conan's pkg cache"
+echo "Mounting conan's nfs pkg cache"
 mkdir -p /mnt/cache/pkg
 mount -v 10.0.0.2:/var/cache/pkg /mnt/cache/pkg
 
 sed -i '' -e 's/^#PKG_CACHEDIR/PKG_CACHEDIR/g' /usr/local/etc/pkg.conf
 sed -i '' -e 's$/var/cache/pkg$/mnt/cache/pkg$g' /usr/local/etc/pkg.conf
+
+mkdir -p /usr/home/vagrant/proj
+cd /usr/home/vagrant/proj
+cp /vagrant/config/composer/composer.json /usr/home/vagrant/proj
+cp /vagrant/config/private-bower/bower.json /usr/home/vagrant/proj
+cp /vagrant/config/private-bower/.bowerrc /usr/home/vagrant/proj
+cp /vagrant/config/sinopia/package.json /usr/home/vagrant/proj
+cp /vagrant/config/sinopia/.npmrc /usr/home/vagrant/
+chown -R vagrant /usr/home/vagrant
+
+echo "Setting npm registry"
+npm set registry "http://10.0.0.2:4873"
 
